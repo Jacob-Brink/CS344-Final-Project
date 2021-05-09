994937e3e218c7b70f22c81cdaa2ddd6333d1f7d
Jacob Brink <jacobpeterbrink@gmail.com>
2021-05-08 23:13:36 -0400

    Fixed custom card

--- src/app/components/minecraft-skin-viewer/minecraft-skin-viewer.component.ts
+++ src/app/components/minecraft-skin-viewer/minecraft-skin-viewer.component.ts
@@ -9,11 +9,10 @@ let skinview3d = require('skinview3d');
 })
 export class MinecraftSkinViewerComponent implements OnInit {
 
-  safeToAdjustSize: boolean = false;
-  viewportWidth: number = 20;
-  viewportHeight: number = 20;
-  minWidth: number = 30*4;
-  minHeight: number = 40*4;
+  viewportWidth: number = 3*8;
+  viewportHeight: number = 4*8;
+  minWidth: number = 30*5;
+  minHeight: number = 40*5;
 
   skinViewer: any = null;
 
@@ -21,26 +20,32 @@ export class MinecraftSkinViewerComponent implements OnInit {
 
   ngAfterViewInit() {
     setInterval(() => {
-      this.safeToAdjustSize = true;
+      this.resizeViewer();
     }, 200);
   }
 
-  @HostListener('window:resize', ['$event'])
-  onResize(e: Event) {
-    console.log("Whee")
+  resizeViewer(): void {
     this.skinViewer.width = window.innerWidth * (this.viewportWidth / 100) + this.minWidth;
     this.skinViewer.height = window.innerWidth * (this.viewportHeight / 100) + this.minHeight;
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(e: Event) {
+    this.resizeViewer();
+  }
+
   // https://github.com/bs-community/skinview3d
   ngOnInit(): void {
+    const skinParts = ["head", "body", "rightArm", "leftArm", "rightLeg", "leftLeg"];
+		const skinLayers = ["innerLayer", "outerLayer"];
+
     this.skinViewer = new skinview3d.SkinViewer({
       canvas: document.getElementById("skin_container"),
-      skin: "/assets/images/skin.png"
+      skin: "/assets/images/skin.jpg"
     });
   
     // Load another skin
-    this.skinViewer.loadSkin("img/skin2.png");
+    this.skinViewer.loadSkin("/assets/images/skin.png");
   
     // Unload(hide) the cape / elytra
     this.skinViewer.loadCape(null);
@@ -48,12 +53,19 @@ export class MinecraftSkinViewerComponent implements OnInit {
     // set the background color
     this.skinViewer.renderer.setClearColor(0x000000);
   
+    
+
+
     // Control objects with your mouse!
     let control = skinview3d.createOrbitControls(this.skinViewer);
     control.enableRotate = false;
     control.enableZoom = false;
     control.enablePan = false;
   
+    
+
+    
+
     // Add an animation
     let walk = this.skinViewer.animations.add(skinview3d.WalkingAnimation);
     // Add another animation
@@ -68,7 +80,7 @@ export class MinecraftSkinViewerComponent implements OnInit {
     // Set the speed of an animation
     run.speed = 0.2;
     // Pause single animation
-    run.paused = true;
+    //run.paused = true;
     // Pause all animations!
     //skinViewer.animations.paused = true;
   }

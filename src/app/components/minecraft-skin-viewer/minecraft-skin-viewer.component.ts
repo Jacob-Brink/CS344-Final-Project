d17ad92e7bd43dafd5b125eef575fa4cb779619d
Jacob Brink <jacobpeterbrink@gmail.com>
2021-05-08 22:28:06 -0400

    Hacked resizing the canvas

--- src/app/components/minecraft-skin-viewer/minecraft-skin-viewer.component.ts
+++ src/app/components/minecraft-skin-viewer/minecraft-skin-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
 let skinview3d = require('skinview3d');
 
@@ -9,48 +9,61 @@ let skinview3d = require('skinview3d');
 })
 export class MinecraftSkinViewerComponent implements OnInit {
 
+  safeToAdjustSize: boolean = false;
+  viewportWidth: number = 20;
+  viewportHeight: number = 20;
+  minWidth: number = 30*4;
+  minHeight: number = 40*4;
+
+  skinViewer: any = null;
+
   constructor() { }
 
   ngAfterViewInit() {
+    setInterval(() => {
+      this.safeToAdjustSize = true;
+    }, 200);
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(e: Event) {
+    console.log("Whee")
+    this.skinViewer.width = window.innerWidth * (this.viewportWidth / 100) + this.minWidth;
+    this.skinViewer.height = window.innerWidth * (this.viewportHeight / 100) + this.minHeight;
+  }
+
+  // https://github.com/bs-community/skinview3d
   ngOnInit(): void {
-    let skinViewer = new skinview3d.SkinViewer({
+    this.skinViewer = new skinview3d.SkinViewer({
       canvas: document.getElementById("skin_container"),
-      width: 300,
-      height: 400,
       skin: "/assets/images/skin.png"
     });
   
-    // Change viewer size
-    skinViewer.width = 500;
-    skinViewer.height = 700;
-  
     // Load another skin
-    skinViewer.loadSkin("img/skin2.png");
+    this.skinViewer.loadSkin("img/skin2.png");
   
     // Unload(hide) the cape / elytra
-    skinViewer.loadCape(null);
+    this.skinViewer.loadCape(null);
     
     // set the background color
-    skinViewer.renderer.setClearColor(0x000000);
+    this.skinViewer.renderer.setClearColor(0x000000);
   
     // Control objects with your mouse!
-    let control = skinview3d.createOrbitControls(skinViewer);
+    let control = skinview3d.createOrbitControls(this.skinViewer);
     control.enableRotate = false;
     control.enableZoom = false;
     control.enablePan = false;
   
     // Add an animation
-    let walk = skinViewer.animations.add(skinview3d.WalkingAnimation);
+    let walk = this.skinViewer.animations.add(skinview3d.WalkingAnimation);
     // Add another animation
-    let rotate = skinViewer.animations.add(skinview3d.RotatingAnimation);
+    let rotate = this.skinViewer.animations.add(skinview3d.RotatingAnimation);
     // Remove an animation, stop walking dude
     //walk.remove();
     // Remove the rotating animation, and make the player face forward
     //rotate.resetAndRemove();
     // And run for now!
-    let run = skinViewer.animations.add(skinview3d.RunningAnimation);
+    let run = this.skinViewer.animations.add(skinview3d.RunningAnimation);
   
     // Set the speed of an animation
     run.speed = 0.2;
